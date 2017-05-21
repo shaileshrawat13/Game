7692487fd9b8a5d1a3d9a3e7f1d00f596fddd1f3
SHAILESH RAWAT <shaileshrawat13@gmail.com>
2017-05-22 00:34:18 +0530

    new

---
+++ app/src/main/java/shaileshrawat/game/Homepage.java
@@ -0,0 +1,16 @@
+package shaileshrawat.game;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by SHAILESH RAWAT on 15-05-2017.
+ */
+
+public class Homepage extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homepage);
+    }
+}
